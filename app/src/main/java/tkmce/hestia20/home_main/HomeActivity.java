080c05c3544c8ff06112aeccf55e5c8f114d5d3b
HyzamAli <hyzamzz@gmail.com>
2020-02-02 14:48:22 +0530

    Added side menu.

---
+++ app/src/main/java/tkmce/hestia20/home_main/HomeActivity.java
@@ -0,0 +1,42 @@
+package tkmce.hestia20.home_main;
+
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import tkmce.hestia20.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        drawer = findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        //drawer.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}

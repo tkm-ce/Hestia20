4aed2bad231d2682ac46bc001a0e1389b9a93fd4
HyzamAli <hyzamzz@gmail.com>
2020-02-11 20:32:09 +0530

    Fixed detailed events

--- app/src/main/java/tkmce/hestia20/Constants.java
+++ app/src/main/java/tkmce/hestia20/Constants.java
@@ -6,13 +6,5 @@ public class Constants {
     public static final String KEY_FULL_NAME = "fullname";
     public static final String KEY_PHONE = "phone";
 
-    interface Radio {
-        int NOTIFICATION_ID = 1958;
-        String CHANNEL_ID = "hestia.radio.channel";
 
-        String STATE = "hestia.radio.state";
-
-        String URL = "http://streaming.radio.co/sd6cafcd5b/listen"; // todo URL here
-
-    }
 }
